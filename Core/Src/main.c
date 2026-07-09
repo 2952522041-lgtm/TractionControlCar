bd839867b10ccd087a9eb783d2a6dd85390f1feb
yusicheng <2952522041@qq.com>
2026-07-09 15:13:42 +0800

    提供循迹模块接入串口

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -92,6 +93,8 @@ int main(void)
   MX_TIM1_Init();
   MX_TIM2_Init();
   MX_TIM4_Init();
+  MX_TIM3_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
   Encoder_Init();
   Car_Init();
